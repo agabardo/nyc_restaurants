61e25e16e7317c13c8f3abc3c19b59356edc4fc9
Ademir C. Gabardo <ademir.gabbardo@gmail.com>
2016-03-05 20:10:30 +1100

    Listing the Restaurants from MongoDB

--- mynyc/app.js
+++ mynyc/app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+
+
 var app = express();
 
 // view engine setup
--- mynyc/routes/index.js
+++ mynyc/routes/index.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
+var MongoClient = require('mongodb').MongoClient;
+var restaurants;
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var ev = {"titulo":"meu titulo","dados":"meus dados"};  
-  res.render('index', {eventData : ev});
+router.get('/', function(req, res, next) {	
+
+	
+	MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
+	  if (err) {
+	    throw err;
+	  }
+	  
+	  var collection = db.collection('restaurants').find().toArray(function(err, result) {
+		    if (err) {
+		        throw err;
+		      }
+		  //var restaurants =  result;
+		  //console.log(result);
+		  restaurants = result;
+	  });
+      
+	});
+	var ev = {"titulo":"meu titulo","dados":"meus dados","restaurants":restaurants};
+	res.render('index', {eventData : ev});
 });
 
 module.exports = router;
