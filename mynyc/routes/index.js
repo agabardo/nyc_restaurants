0d8b0cd19efa4dc800732d054a7a358c9382eaaa
Ademir C. Gabardo <ademir.gabbardo@gmail.com>
2016-03-11 09:10:09 +1100

    Just updating

--- mynyc/routes/index.js
+++ mynyc/routes/index.js
@@ -1,31 +1,25 @@
-//var express = require('express');
-
 var express = require('express'),
 router = express.Router(),
 mongoose = require('mongoose'), //mongo connection
 bodyParser = require('body-parser'), //parses information from POST
 methodOverride = require('method-override'); //used to manipulate POST
 
-//var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var restaurants;
 var restaurants2;
-/* GET home page. */
-router.get('/', function(req, res, next) {	
 
-	
+
+
+router.get('/', function(req, res, next) {
 	MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
 	  if (err) {
 	    throw err;
 	  }
-	  
 	  var collection = db.collection('restaurants').find().toArray(function(err, result) {
 		  if (err) {throw err;}else{restaurants = result;}
 	  });
-      
 	});
-	var ev = {"titulo":"meu titulo","dados":"meus dados","restaurants":restaurants};
-	
+	var ev = {"titulo":"meu titulo","dados":"meus dados","restaurants":restaurants}; 
 	res.render('index', {eventData : ev});
 });
 
--- readme.md
+++ readme.md
@@ -4,4 +4,5 @@ Repository with the first experiments with MongoDB, Express, Angular and Node.JS
 
 Goal: With the example dataset of NYC restaurants build an small web search page to find restaurants in NYC.
 Status: in progress...
+
 Pending: read the data from database and send to the Javascript on the header..
