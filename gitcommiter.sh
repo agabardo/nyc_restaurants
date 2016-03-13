528c69cf64d634db6c6a565e75fdaa99e16cbd5b
Ademir C. Gabardo <ademir.gabbardo@gmail.com>
2016-03-13 11:19:33 +1100

    Just an automatic commit from a bash script! Yes, I'm lazzy at this time.. or, May the coffee is gone. Probably regreting in the future!

--- gitcommiter.sh
+++ gitcommiter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m "Just an automatic commit from a bash script!"
+git commit -m "Just an automatic commit from a bash script! Yes, I'm lazzy at this time.. or, May the coffee is gone. Probably regreting in the future!"
 git push origin master
