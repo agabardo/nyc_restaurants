681eac12a6768c5e0292f8399310cc64ac30d41e
Ademir C. Gabardo <ademir.gabbardo@gmail.com>
2016-03-25 15:15:31 +1100

    Just an update, start working with full text search

--- mynyc/app.js
+++ mynyc/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var search = require('./routes/search');
 var db = require('./model/db');
 var restaurants = require('./model/restaurants');
 
@@ -19,8 +20,8 @@ app.set('view engine', 'jade');
 // app.engine('html', require('ejs').renderFile);
 // app.set('view engine', 'html');
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/search', search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ mynyc/routes/search.js
@@ -0,0 +1,50 @@
+var express = require('express'), router = express.Router(), mongoose = require('mongoose'), // mongo connection
+bodyParser = require('body-parser'), // parses information from POST
+methodOverride = require('method-override'); // used to manipulate POST
+router.get('/', function(req, res, next) {
+
+	var boroughs;
+	var cuisine = "All";
+	mongoose.model('Restaurants').distinct("borough", function(err, resBorough){
+		if (err) {
+			return console.error(err);
+		} 
+		else{
+			boroughs = resBorough;
+		}
+	});
+	
+	var filter = {$text:{$search: "Dog"}};
+	
+	mongoose.model('Restaurants').distinct("cuisine", function(err, cuisinesList) {
+		if (err) {
+			return console.error(err);
+		} 
+		else {
+
+			mongoose.model('Restaurants').find(filter, function(err, restaurants) {
+				if (err) {
+					return console.error(err);
+				} 
+				else{
+					res.format({
+						html : function() {
+							res.render('index', {
+								title : 'NYC Restaurants',
+								"cuisines_list" : cuisinesList,
+								"boroughs_list" : boroughs,
+								cuisine : cuisine,
+								"restaurants2" : JSON.stringify(restaurants),
+								"restaurants" : restaurants});
+							}, 
+							json : function() { // JSON response
+								//res.json(restaurants, cuisinesList);
+							}
+					});
+				}
+			}).limit(null);
+		}
+	});
+	
+});
+module.exports = router;
