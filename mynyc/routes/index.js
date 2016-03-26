d8fe5c98cd2653d571a191ed5194b104a28c33b4
Ademir C. Gabardo <ademir.gabbardo@gmail.com>
2016-03-26 18:56:07 +1100

    Some improvements

--- mynyc/routes/index.js
+++ mynyc/routes/index.js
@@ -2,65 +2,59 @@ var express = require('express'), router = express.Router(), mongoose = require(
 bodyParser = require('body-parser'), // parses information from POST
 methodOverride = require('method-override'); // used to manipulate POST
 router.get('/', function(req, res, next) {
-	var filter;
-	var cuisine;
+	
+	var filter = {};
+	var cuisine = "All";
 	var cuisinesList;
 	var boroughs;
 
-	if (req.query.cuisine && req.query.cuisine !="All") {
+	if(req.query.borough){
+		filter = {cuisine : req.query.cuisine, borough : req.query.borough};
+	}
+	if(req.query.cuisine && req.query.cuisine !="All") {
 		filter = {cuisine : req.query.cuisine};
-		cuisine = req.query.cuisine;
-		
+		cuisine = req.query.cuisine;	
 		if(req.query.borough){
 			filter = {cuisine : req.query.cuisine, borough : req.query.borough};
 		}
-	} 
-	else {
-		filter = {};
-		if(req.query.borough){
-			filter = {cuisine : req.query.cuisine, borough : req.query.borough};
-		}
-		cuisine = "All";
 	}
 	
-	mongoose.model('Restaurants').distinct("borough", function(err, resBorough){
-		if (err) {
-			return console.error(err);
-		} 
-		else{
-			boroughs = resBorough;
-		}
-	});
+	const findBorough = () => {
+		mongoose.model('Restaurants').distinct("borough", function(err, resBorough){
+			if (err) {return console.error(err);} 
+			else{boroughs = resBorough;}
+		});
+	}
 
-	mongoose.model('Restaurants').distinct("cuisine", function(err, cuisinesList) {
-		if (err) {
-			return console.error(err);
-		} 
-		else {
-			//console.log(cuisinesList);
-			mongoose.model('Restaurants').find(filter, function(err, restaurants) {
-				if (err) {
-					return console.error(err);
-				} 
-				else{
-					res.format({
-						html : function() {
-							res.render('index', {
-								title : 'NYC Restaurants',
-								"cuisines_list" : cuisinesList,
-								"boroughs_list" : boroughs,
-								cuisine : cuisine,
-								"restaurants2" : JSON.stringify(restaurants),
-								"restaurants" : restaurants});
-							}, 
-							json : function() { // JSON response
-								//res.json(restaurants, cuisinesList);
-							}
-					});
-				}
-			}).limit(null);
+	const listCuisines = () => {
+		mongoose.model('Restaurants').distinct("cuisine", function(err, resCuisinesList) {
+			if (err) {return console.error(err);} 
+			else {cuisinesList = resCuisinesList;}
+		});
+	}
+			
+	mongoose.model('Restaurants').find(filter, function(err, resRestaurants) {
+		if (err) {return console.error(err);} 
+		else{
+			res.format({
+				html : function() {
+					res.render('index', {
+						title : 'NYC Restaurants',
+						"cuisines_list" : cuisinesList,
+						"boroughs_list" : boroughs,
+						"cuisine"		: cuisine,
+						"restaurants2" 	: JSON.stringify(resRestaurants),
+						"restaurants" 	: resRestaurants});
+					}, 
+					json : function() { // JSON response
+						//res.json(restaurants, cuisinesList);
+					}
+			});
 		}
-	});
+	}).limit(null);
+	
+	listCuisines();
+	findBorough();
 
 });
 module.exports = router;
