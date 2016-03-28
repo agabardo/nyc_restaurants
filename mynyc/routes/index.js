ed40a9ef3098918a7d0ca886311c16bc2dc7d9b0
Ademir C. Gabardo <ademir.gabbardo@gmail.com>
2016-03-28 18:20:18 +1100

    Adding promisses to the project.

--- mynyc/routes/index.js
+++ mynyc/routes/index.js
@@ -1,8 +1,8 @@
-var express = require('express'), router = express.Router(), mongoose = require('mongoose'), // mongo connection
-bodyParser = require('body-parser'), // parses information from POST
-methodOverride = require('method-override'); // used to manipulate POST
+var express = require('express'), router = express.Router(), mongoose = require('mongoose'),
+bodyParser = require('body-parser'),
+methodOverride = require('method-override');
 router.get('/', function(req, res, next) {
-	
+
 	var filter = {};
 	var cuisine = "All";
 	var cuisinesList;
@@ -11,31 +11,36 @@ router.get('/', function(req, res, next) {
 	if(req.query.borough){
 		filter = {cuisine : req.query.cuisine, borough : req.query.borough};
 	}
-	if(req.query.cuisine && req.query.cuisine !="All") {
+	if(req.query.cuisine){
 		filter = {cuisine : req.query.cuisine};
-		cuisine = req.query.cuisine;	
+		cuisine = req.query.cuisine;
 		if(req.query.borough){
 			filter = {cuisine : req.query.cuisine, borough : req.query.borough};
 		}
 	}
-	
-	const findBorough = () => {
+
+
+	findBorough = () => {
 		mongoose.model('Restaurants').distinct("borough", function(err, resBorough){
-			if (err) {return console.error(err);} 
+			if (err) {return console.log(err);}
 			else{boroughs = resBorough;}
 		});
 	}
 
-	const listCuisines = () => {
+	listCuisines = () => {
 		mongoose.model('Restaurants').distinct("cuisine", function(err, resCuisinesList) {
-			if (err) {return console.error(err);} 
+			if (err) {return console.log(err);}
 			else {cuisinesList = resCuisinesList;}
 		});
 	}
-			
+
+	findBorough();
+	listCuisines();
+
 	mongoose.model('Restaurants').find(filter, function(err, resRestaurants) {
-		if (err) {return console.error(err);} 
+		if (err) {return console.error(err);}
 		else{
+
 			res.format({
 				html : function() {
 					res.render('index', {
@@ -45,16 +50,13 @@ router.get('/', function(req, res, next) {
 						"cuisine"		: cuisine,
 						"restaurants2" 	: JSON.stringify(resRestaurants),
 						"restaurants" 	: resRestaurants});
-					}, 
+					},
 					json : function() { // JSON response
 						//res.json(restaurants, cuisinesList);
 					}
 			});
 		}
 	}).limit(null);
-	
-	listCuisines();
-	findBorough();
 
 });
 module.exports = router;
