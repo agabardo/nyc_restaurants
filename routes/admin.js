5641de69f1017c32ebce48cafcd3ea54ecf6b5f9
Ademir C. Gabardo <ademir.gabbardo@gmail.com>
2016-07-23 20:46:53 +1000

    Adding the Alexander Pierce's AdminLTE administration template to the admin area

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
-
+var express = require('express'), router = express.Router(), mongoose = require('mongoose'),
+bodyParser = require('body-parser'),
+methodOverride = require('method-override'),
+Promise = require('bluebird');
 
 router.get('/', function(req, res, next) {
-  res.send('Administration area');
+  var restaurantsModel = mongoose.model('Restaurants');
+	console.log("Oi");
+	Promise.promisifyAll(mongoose);
+	Promise.props({
+		title 			: 'NYC Restaurants - Administration',
+		//boroughs_list	: restaurantsModel.distinct("borough").execAsync(),
+		//cuisines_list	: restaurantsModel.distinct("cuisine").execAsync(),
+		//grades			: restaurantsModel.distinct("grades.grade").execAsync(),
+		//restaurants		: restaurantsModel.find(filter,restaurant_fields).limit(100).execAsync(),
+		//lower			: "1", //restaurantsModel.find({},{"grades.score":1}).sort({"grades.score":1}).limit(1)
+		//upper			: restaurantsModel.find({},{"grades.score":1}).sort({"grades.score":-1}).limit(1),
+		//"cuisine" 		: cuisine,
+		//"borough"		: borough,
+		//"grade"			: grade
+	}).then(function(results) {
+		//res.json(results);
+		console.log(results);
+	  res.render('admin/index', results);
+	}).catch(function(err) {
+		console.log(err);
+	    res.send(500); // oops - we're even handling errors!
+	});
 });
-
 module.exports = router;
